781a29c76895e8330d9a7883634b6444d997b70b
STBMTE <ateruhin388@gmail.com>
2021-12-07 10:16:09 +0300

    12

--- ThirdPractice/ThirdPractice/MyList.cs
+++ ThirdPractice/ThirdPractice/MyList.cs
@@ -93,5 +93,135 @@ namespace ThirdPractice
             }
             return Output;
         }
+
+        public void AddList(List<T> data)
+        {
+            for(int i = 0; i < data.Count; i++)
+            {
+                Add(data[i]);
+            }
+        }
+        public void Razbienie(T data)
+        {
+            var node = _Head;
+            MyList<T> list = new MyList<T>();
+            MyList<T> list1 = new MyList<T>();
+            for (int i = 0; i < Count; i++)
+            {
+                if (node.Value.Equals(data))
+                {
+                    list1.Add(node.Value);
+                }
+                else
+                {
+                    list.Add(node.Value);
+                }
+                node = node.NextValue;
+            }
+        }
+        public void Swap(T dataA, T dataB)
+        {
+            var node = _Head;
+            Node<T> nodeA = null;
+            Node<T> nodeB = null;
+            for (int i = 0; i < Count; i++)
+            {
+                if (node.Value.Equals(dataA))
+                {
+                    nodeA = node;
+                }
+                if (node.Value.Equals(dataB))
+                {
+                    nodeB = node;
+                }
+                node = node.NextValue;
+            }
+            if(nodeA == null || nodeB == null)
+            {
+                throw new Exception("No element");
+            }
+            else
+            {
+                var nodea = new Node<T>(nodeA.Value);
+                var nodeb = new Node<T>(nodeB.Value);
+                nodea.NextValue = nodeB.NextValue;
+                nodeb.NextValue = nodeA.NextValue;
+                nodea.PrevValue = nodeB.PrevValue;
+                nodeb.PrevValue = nodeA.PrevValue;
+                nodeA.PrevValue.NextValue = nodeb;
+                nodeA.NextValue.PrevValue = nodeb;
+                nodeB.PrevValue.NextValue = nodea;
+                nodeB.NextValue.PrevValue = nodea;
+            }
+        }
+        public void Insert(T ElementE, T ElementF)
+        {
+            var node = _Head;
+            var elementF = new Node<T>(ElementF);
+            for(int i = 0; i < Count; i++)
+            {
+                if (node.Value.Equals(ElementE))
+                {
+                    break;
+                }
+                node = node.NextValue;
+            }
+            elementF.NextValue = node;
+            elementF.PrevValue = node.PrevValue;
+            node.PrevValue.NextValue = elementF;
+            /*elementF.NextValue = node;
+            elementF.PrevValue = node.PrevValue;
+            *//*elementF.PrevValue.NextValue = elementF;*/
+            /*node.PrevValue = elementF;*//*
+            node.PrevValue.NextValue = elementF;*/
+        }
+        public void RemovingSecondRefNode(T data)
+        {
+            var node = _Head;
+            List<Node<T>> listNode = new List<Node<T>>();
+            for(int i = 0; i < Count; i++)
+            {
+                if (node.Value.Equals(data))
+                {
+                    listNode.Add(node);
+                }
+                node = node.NextValue;
+            }
+            var removedNode = listNode[1];
+            removedNode.NextValue.PrevValue = removedNode.PrevValue;
+            removedNode.PrevValue.NextValue = removedNode.NextValue;
+            Count--;
+        }
+        public void RemoveRefNode(T data)
+        {
+            var node = _Head;
+            List<Node<T>> listNode = new List<Node<T>>();
+            Console.WriteLine(ToString());
+            for (int i = 0; i < Count; i++)
+            {
+                if (node.Value.Equals(data))
+                {
+                    listNode.Add(node);
+                }
+                node = node.NextValue;
+            }
+            for (int i = 0; i < listNode.Count; i++)
+            {
+                var removedNode = listNode[i];
+                removedNode.NextValue.PrevValue = removedNode.PrevValue;
+                removedNode.PrevValue.NextValue = removedNode.NextValue;
+                Count--;
+            }
+        }
+        public void Doubling()
+        {
+            var node = _Head;
+            int x = Count;
+            for(int i = 0; i < x; i++)
+            {
+                Add(node.Value);
+                node = node.NextValue;
+            }
+        }
     }
 }
--- ThirdPractice/ThirdPractice/Program.cs
+++ ThirdPractice/ThirdPractice/Program.cs
@@ -13,6 +13,11 @@ namespace ThirdPractice
             list.Add(4);
             list.Add(5);
             list.Add(6);
+            list.Swap(2, 4);
+            list.Doubling();
+            list.RemoveRefNode(2);
+            list.RemovingSecondRefNode(1);
+            list.Insert(5, 8);
             Console.WriteLine(list.ToString());
         }
     }
